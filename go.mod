117dda83f44e8522f1a71aef92f00b40e70e99e0
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-10-19 11:00:46 +0200

    Additions

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.19
 require (
 	github.com/iVitaliya/colors-go v0.0.0-20220811123250-641c37bf0b3d // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2 // indirect
 )
--- utilities/utils.go
+++ utilities/utils.go
@@ -2,26 +2,115 @@ package utils
 
 import (
 	"fmt"
+	"strings"
+	"gopkg.in/toast.v1"
 )
 
-func Stringify[T any](value T) string {
-	return fmt.Sprint(value)
+// Returns a value to a type of string
+//
+// Usage: utilities.JoinArray[Type](Value of type Type, One Line Stringify)
+//
+// Example:
+//
+//	numb := 25
+//	str := utilities.Stringify[int](numb, false)
+//	fmt.Print(str) // "25"
+func Stringify[T any](value T, one_line bool) string {
+	var str string
+
+	if !one_line {
+		str = fmt.Sprint(value)
+	} else {
+		str = fmt.Sprintln(value)
+	}
+
+	return str
 }
 
-func JoinArray[T any](array []T) string {
+// Returns a string which is joined by item & seperator to seperate each item from each other by a string.
+//
+// Usage: utilities.JoinArray[Type](Array of type Type, Seperator)
+//
+// Example:
+//
+//	arr := []int{ 1, 3, 7, 4, 6, 8, 1 }
+//	str := utilities.JoinArray[int](arr, "\n")
+//	fmt.Print(str) /* "1
+//	3
+//	7
+//	4
+//	6
+//	8
+//	1" */
+func JoinArray[Type any](array []Type, seperator string) string {
 	var result string
 	for i := 0; i < len(array); i++ {
-		length := len(array)-1
+		length := len(array) - 1
 		if i == length {
 			result = result + fmt.Sprint(array[i])
 		}
 
-		result = result + fmt.Sprint(array[i]) + " "
+		result = result + fmt.Sprint(array[i]) + seperator
 	}
 
 	return result
 }
 
-func AppendArray[T]() {
+// Appends an item to the specified array using the specified value.
+//
+// Usage: utilities.AppendArray[Type](Array of type Type, Value of type Type)
+//
+// Example:
+//
+//	arr1 := []int{ 1, 3, 7, 4, 6, 8, 1 }
+//	arr2 := utilities.AppendArray[int](arr1, 3)
+//	fmt.Print(arr2) // 1, 3, 7, 4, 6, 8, 1, 3
+func AppendArray[T any](array []T, value T) []T {
+	var arr []T = append(array, value)
 
+	return arr
+}
+
+// Formats a partition if it doesn't include ":", else, return the given partition.
+//
+// Usage: utilities.FormatPartition(Partition)
+//
+// Example:
+// 	partition := "c"
+// 	formatted := utilities.FormatPartition(partition)
+//	fmt.Print(formatted) // c:
+func FormatPartition(partition string) string {
+	var part string 
+	
+	if strings.Contains(partition, ":") {
+		part = partition
+	} else {
+		part = partition + ":"
+	}
+
+	return part
+}
+
+func SendFinishedNotification(command string) {
+	notification := toast.Notification{
+		AppID: "Commander",
+		Title: "Commander | Commando Uitgevoerd",
+		Message: command + " is klaar, wil je nog een commando uitvoeren",
+		Icon: "icon.png",
+		Actions: []toast.Action{
+			{
+				Type: "protocol", 
+				Label: "Ja, graag", 
+				Arguments: ""
+			},
+			{Type: "protocol", Label: "Nee, ik ben klaar", Arguments: ""},
+		},
+	}
+
+	RunButtonAction(notification.Actions[0])
+	RunButtonAction(notification.Actions[1])
+}
+
+func RunButtonAction(button toast.Action) {
+	
 }
